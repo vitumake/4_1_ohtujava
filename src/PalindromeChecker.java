edab9bc281d6af8c3e6508228fe8b90776c556d4
markus <mag3768@gmail.com>
2023-09-12 11:14:25 +0300

    fixed all the erors

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class PalindromeChecker {
 
+
+    
+    public static String transformInput(String input) {
+        input = input.toLowerCase();
+        input = input.replace(",", "");
+        input = input.replace("!", "");
+        return input;
+    }
+    
+    public static boolean checkPalindrome(String input) {
+        String reversed = reverseString(input);
+        if (input.equals(reversed)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public static String reverseString(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
@@ -13,29 +40,7 @@ public class PalindromeChecker {
         } else {
             System.out.println("The transformed input is not a palindrome.");
         }
-    }
 
-    public static String transformInput(String input) {
-        input = input.toLowerCase();
-        input = input.replace(",", "");
-        input = input.replace("!", "");
-        return input;
-    }
-
-    public static boolean checkPalindrome(String input) {
-        String reversed = reverseString(input);
-        if (input.equals(reversed)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static String reverseString(String input) {
-        StringBuilder reversed = new StringBuilder();
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed.append(input.charAt(i));
-        }
-        return reversed.toString();
+        scanner.close();
     }
 }
