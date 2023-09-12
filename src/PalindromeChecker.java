8e7448e9c32914eef6b96f7da53996848cdbf1ec
Samu Hiltunen <samu.hiltunen@metropolia.fi>
2023-09-12 11:14:52 +0300

    refactor

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
+
 public class PalindromeChecker {
 
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
         String input = scanner.nextLine();
+        scanner.close();
         
         String transformed = transformInput(input);
         
-        boolean isPalindrome = checkPalindrome(transformed);
-        if (isPalindrome) {
+        if (isPalindrome(transformed)) {
             System.out.println("The transformed input is a palindrome.");
         } else {
             System.out.println("The transformed input is not a palindrome.");
@@ -16,26 +18,11 @@ public class PalindromeChecker {
     }
 
     public static String transformInput(String input) {
-        input = input.toLowerCase();
-        input = input.replace(",", "");
-        input = input.replace("!", "");
-        return input;
+        return input.toLowerCase().replaceAll("[,!]", "");
     }
 
-    public static boolean checkPalindrome(String input) {
-        String reversed = reverseString(input);
-        if (input.equals(reversed)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static String reverseString(String input) {
-        StringBuilder reversed = new StringBuilder();
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed.append(input.charAt(i));
-        }
-        return reversed.toString();
+    public static boolean isPalindrome(String input) {
+        String reversed = new StringBuilder(input).reverse().toString();
+        return input.equals(reversed);
     }
 }
